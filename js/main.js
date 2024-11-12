6ca5f03b97c73993ef3ca7677ea83c4068109b7e
Usagitakachi <usagiloverion@gmail.com>
2024-11-13 02:50:01 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,29 @@ function toggleMobileMenu() {
   $('.btn-mobile-menu__icon').toggleClass('hidden');
   $('.btn-mobile-close__icon').toggleClass('hidden');
 }
+// 鼠标点击事件
+document.addEventListener('click', (event) => {
+  // 创建文字元素
+  const text = document.createElement('span');
+  text.textContent = '智乃'; // 显示的文字内容
+  text.classList.add('chino-text'); // 添加类名以应用样式
+
+  // 设置文字出现的位置
+  text.style.left = `${event.pageX}px`;
+  text.style.top = `${event.pageY}px`;
+
+  // 将文字元素添加到页面
+  document.body.appendChild(text);
+
+  // 设置动画效果：向上移动并淡出
+  setTimeout(() => {
+    text.style.opacity = '0'; // 渐隐
+    text.style.transform = 'translateY(-20px)'; // 向上移动20像素
+  }, 0);
+
+  // 在1秒后删除文字元素
+  setTimeout(() => text.remove(), 1000);
+});
 
 $(document).ready(function () {
   $('a.panel-button').click(function (e) {
